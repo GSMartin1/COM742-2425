8c2d6fb36dccfe5e0ac9135663167ea4b377f2fc
George <gs.martin@ulster.ac.uk>
2025-02-20 15:41:13 +0000

    //Week4b sols

---
+++ Week4b/src/factorials.java
@@ -0,0 +1,13 @@
+public class factorials {
+    public static void main(String[] args){
+        System.out.println(factorials(5));
+    }
+    public static int factorials(int n) {
+        if (n == 1){
+            return 1;
+        }
+       else {
+            return n * factorials(n - 1);
+        }
+    }
+}
